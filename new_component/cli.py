32e0c8e98825ed68d694ca74ea7fabfc81de996e
icancclearynow <hello@iancleary.me>
2022-02-01 21:43:16 -0700

    Added new-component script

--- new_component/cli.py
+++ new_component/cli.py
@@ -1,10 +1,14 @@
+from pathlib import Path
 from typing import Optional
 
 import typer
 
+import new_component
 from new_component import __app_name__, __version__
 
 app = typer.Typer()
+
+__COMPONENTS_DIR__ = "src/components/"
 
 
 def _version_callback(value: bool) -> None:
@@ -13,8 +17,13 @@ def _version_callback(value: bool) -> None:
         raise typer.Exit()
 
 
-@app.callback()
+@app.command()
 def main(
+    name: str = typer.Argument(
+        None,
+        help="Name of component to create.",
+        # callback=_create_component
+    ),
     version: Optional[bool] = typer.Option(
         None,
         "--version",
@@ -22,6 +31,18 @@ def main(
         help="Show the application's version and exit.",
         callback=_version_callback,
         is_eager=True,
-    )
+    ),
 ) -> None:
-    return
+
+    components_directory = Path(__COMPONENTS_DIR__)
+
+    new_directory = components_directory / name
+
+    if new_directory.exists() is False:
+        new_directory.mkdir(
+            parents=True
+        )  # Create directory, creating missing parent folders
+
+    # index_file = new_directory / "index.js"
+
+    print(new_component.__file__)

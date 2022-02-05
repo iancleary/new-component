00d65f19ffab94a9729b2c4844f0be9149876f39
iancleary <hello@iancleary.me>
2022-02-04 17:17:52 -0700

    Add documentation on configuration files

--- new_component/_constants.py
+++ new_component/_constants.py
@@ -9,7 +9,7 @@ INSTALLED_LOCATION = new_component.__file__
 TEMPLATES_DIR = INSTALLED_LOCATION.replace("__init__.py", "")
 TEMPLATES_PATH = Path(TEMPLATES_DIR) / "templates"
 
-CONFIG_FILE = ".new-component.json"
+CONFIG_FILE = ".new-component-config.json"
 LOCAL_CONFIG_FILE = Path(f"./{CONFIG_FILE}")
 
 GLOBAL_CONFIG_FILE = Path("~/.config/new-component/settings.json")
